226b9e2ea60906fcada7e0678b8a39becbccd088
liwwtj <37742545+liwwtj@users.noreply.github.com>
2018-04-21 22:52:47 +0800

    更新了新的代码

---
+++ TIJ/src/containers/CountedString.java
@@ -0,0 +1,54 @@
+package containers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static net.mindview.util.Print.*;
+
+public class CountedString {
+	private static List<String> created = 
+			new ArrayList<>();
+	private String s;
+	private int id = 0;
+	public CountedString(String str){
+		s = str;
+		created.add(s);
+		
+		for (String s2 : created) {
+			if(s2.equals(s))
+				id++;
+		}
+	}
+	public String toString(){
+		return "String: "+s+" id:"+id+
+				" hashCode(): "+hashCode();
+	}
+	public int hashCode(){
+		int result = 17;
+		result = 37*result+s.hashCode();
+		result = 37*result +id;
+		return result;
+	}
+	public boolean equals(Object o){
+		return o instanceof CountedString &&
+				s.equals(((CountedString)o).s) &&
+				id == ((CountedString)o).id;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<CountedString, Integer> map =
+				new HashMap<>();
+		CountedString[] cs = new CountedString[5];
+		for (int i = 0; i < cs.length; i++) {
+			cs[i] = new CountedString("hi");
+			map.put(cs[i], i);
+		}
+		print(map);
+		for (CountedString cstring : cs) {
+			print("Looking up "+cstring);
+			print(map.get(cstring));
+		}
+	}
+
+}
